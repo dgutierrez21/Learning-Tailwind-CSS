ece2dbcc95c3b83c72a4f33b5c1e62d6cf7a8ef2
Dixon G <dxxn0101@gmail.com>
2023-03-14 12:28:10 -0600

    actualizar el renombrador y añadir la plantilla de carpetas y archivos

--- renamer.js
+++ renamer.js
@@ -1,14 +1,19 @@
-export function renombrar(texto) {
+function renombrar(texto) {
   const aEspacio = texto.replaceAll("-", " ");
   const aGuion = texto.replaceAll(" ", "-");
 
   return texto.includes("-") ? aEspacio : aGuion;
 }
 
-export function camelizar(string) {
+function camelizar(string) {
   return string
     .replace(/(?:^\w|[A-Z]|\b\w)/g, function (palabra) {
       return palabra.toUpperCase();
     })
     .replace(/\s+/g, "");
 }
+
+const nombre = "folder and file template";
+
+console.log(renombrar(nombre));
+console.log(camelizar(nombre));
